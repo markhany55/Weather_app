28b986531f3dd0b6bf9286be68af94f86fe37447
markhany55 <markhany5000@gmail.com>
2021-12-23 07:37:44 +0200

    sass

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Fragment } from 'react/cjs/react.production.min';
 import WeatherApp from './Weather app/WeatherApp';
 function App() {
   return (
-    <div >
+    <Fragment >
+
       <WeatherApp/>
-    </div>
+    </Fragment>
   );
 }
 
--- src/Weather app/WeatherApp.js
+++ src/Weather app/WeatherApp.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import {  useFormik } from "formik";
+import './WeatherApp.scss'
 const WeatherApp =()=>{
-    const api_key="373d80a947d6a9e014d4c204527b3348"
 const [weather,setweather]=useState([
 ])
+
 const formik = useFormik({
     initialValues:{
         city:'',
@@ -13,23 +14,21 @@ onSubmit:async (values)=>{
 const link= await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${formik.values.city}%2C${formik.values.country}&appid=373d80a947d6a9e014d4c204527b3348`)
 const data=await link.json('')
 setweather([...weather,{city:data.name,country:data.sys.country,tempreature:data.main.temp,humidity:data.main.humidity,description:data.weather[0].description}])
-console.log(data)
 }
 })
 const show =weather.map((ele,id)=>{
     return(
-        <div key={id}>
-<p>city: {ele.city}</p>
-<p>country: {ele.country}</p>
-<p>tempreature: {ele.tempreature}</p>
-<p>humidity: {ele.humidity}</p>
-<p>conditions: {ele.description}</p>
+        <div className="result" key={id}>
+<p>City: {ele.city}</p>
+<p>Country: {ele.country}</p>
+<p>Tempreature: {ele.tempreature}</p>
+<p>Humidity: {ele.humidity}</p>
+<p>Conditions: {ele.description}</p>
         </div>
     )
 })    
-
 return(
-    <div>
+    <div className="contain">
             <form onSubmit={formik.handleSubmit}>
                 <input type="text" id="city" placeholder="City" onChange={formik.handleChange} />
                 <input type="text" id="country" placeholder="Country" onChange={formik.handleChange} />
